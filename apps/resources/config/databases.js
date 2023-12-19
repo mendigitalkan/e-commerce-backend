c3c86a0015c0547ec5cbcb9fb7fe046e9a6c1522
misdarmanto <misdarmanto1922@gmail.com>
2023-12-19 17:26:14 +0700

    +

--- apps/resources/config/databases.js
+++ apps/resources/config/databases.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: 'root',
     password: 'toor',
-    database: 'e-commerce',
+    database: 'e_commerce_db',
     host: '127.0.0.1',
     dialect: 'mysql'
   },
